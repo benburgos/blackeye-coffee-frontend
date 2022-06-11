16d38cb7b8b81856eb7dbd3856882cff50ebc906
Ben <ben.a.burgos@gmail.com>
2022-06-11 16:21:24 -0700

    enlarges main link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 export default function Header(props) {
-    return (
-        < nav className="nav">
-            <Link to="/">
-                <div>black eye coffee  app</div>
-            </Link>
-
-        </nav>
-    )
+  return (
+    <nav className="nav">
+      <Link to="/">
+        <div>
+          <h1>black eye coffee app</h1>
+        </div>
+      </Link>
+    </nav>
+  );
 }
